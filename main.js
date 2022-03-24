107fdb179cdb5a992b2656b340ac9195e112e666
Vincentfernandes17 <vincentfernandes1708@gmail.com>
2022-03-24 22:39:29 +0700

    = merapikan pages

--- main.js
+++ main.js
@@ -46,44 +46,44 @@ window.onscroll = function() {
 //var bb = document.getElementsByName("email").value;
 //var cc = document.getElementsByName("pesan").value;
 
+//  let home = document.querySelector('#home');
+//let Profil = document.querySelector('#Profil');
+//let resume = document.querySelector('#Portfolio');
+//let project = document.querySelector('#Projects');
+//let contact = document.querySelector('#Contact');
 
-let home = document.querySelector('#home');
-let Profil = document.querySelector('#Profil');
-let resume = document.querySelector('#Portfolio');
-let project = document.querySelector('#Projects');
-let contact = document.querySelector('#Contact');
-
-window.addEventListener('scroll', () => {
-  var windo = window.pageYOffset;
-  if (home.offsetTop - 800 <= windo && Profil.offsetTop - 100 > windo) {
-    document.querySelector('.home').setAttribute('id', 'active');
-    document.querySelector('.Profil').removeAttribute('id', 'active');
-    document.querySelector('.resume').removeAttribute('id', 'active');
-    document.querySelector('.project').removeAttribute('id', 'active');
-    document.querySelector('.contact').removeAttribute('id', 'active');
-  } else if (Profil.offsetTop - 800 <= windo && resume.offsetTop - 500 > windo) {
-    document.querySelector('.home').removeAttribute('id', 'active');
-    document.querySelector('.Profil').setAttribute('id', 'active');
-    document.querySelector('.resume').removeAttribute('id', 'active');
-    document.querySelector('.project').removeAttribute('id', 'active');
-    document.querySelector('.contact').removeAttribute('id', 'active');
-  } else if (resume.offsetTop - 800 <= windo && project.offsetTop - 500 > windo) {
-    document.querySelector('.home').removeAttribute('id', 'active');
-    document.querySelector('.Profil').removeAttribute('id', 'active');
-    document.querySelector('.resume').setAttribute('id', 'active');
-    document.querySelector('.project').removeAttribute('id', 'active');
-    document.querySelector('.contact').removeAttribute('id', 'active');
-  } else if (project.offsetTop - 800 <= windo && contact.offsetTop - 300 > windo) {
-    document.querySelector('.home').removeAttribute('id', 'active');
-    document.querySelector('.Profil').removeAttribute('id', 'active');
-    document.querySelector('.resume').removeAttribute('id', 'active');
-    document.querySelector('.project').setAttribute('id', 'active');
-    document.querySelector('.contact').removeAttribute('id', 'active');
-  } else if (contact.offsetTop - 800 <= windo) {
-    document.querySelector('.home').removeAttribute('id', 'active');
-    document.querySelector('.Profil').removeAttribute('id', 'active');
-    document.querySelector('.resume').removeAttribute('id', 'active');
-    document.querySelector('.project').removeAttribute('id', 'active');
-    document.querySelector('.contact').setAttribute('id', 'active');
-  }
-});
+//window.addEventListener('scroll', () => {
+//var windo = window.pageYOffset;
+//if (home.offsetTop - 800 <= windo && Profil.offsetTop - 100 > windo) {
+//document.querySelector('.home').setAttribute('id', 'active');
+//document.querySelector('.Profil').removeAttribute('id', 'active');
+//document.querySelector('.resume').removeAttribute('id', 'active');
+//document.querySelector('.project').removeAttribute('id', 'active');
+//document.querySelector('.contact').removeAttribute('id', 'active');
+//}
+//else if (Profil.offsetTop - 800 <= windo && resume.offsetTop - 500 > windo) {
+//document.querySelector('.home').removeAttribute('id', 'active');
+//document.querySelector('.Profil').setAttribute('id', 'active');
+//document.querySelector('.resume').removeAttribute('id', 'active');
+//document.querySelector('.project').removeAttribute('id', 'active');
+//document.querySelector('.contact').removeAttribute('id', 'active');
+//} else if (resume.offsetTop - 800 <= windo && project.offsetTop - 500 > windo) {
+//document.querySelector('.home').removeAttribute('id', 'active');
+//document.querySelector('.Profil').removeAttribute('id', 'active');
+//document.querySelector('.resume').setAttribute('id', 'active');
+//document.querySelector('.project').removeAttribute('id', 'active');
+//document.querySelector('.contact').removeAttribute('id', 'active');
+//} else if (project.offsetTop - 800 <= windo && contact.offsetTop - 300 > windo) {
+//document.querySelector('.home').removeAttribute('id', 'active');
+//document.querySelector('.Profil').removeAttribute('id', 'active');
+//document.querySelector('.resume').removeAttribute('id', 'active');
+//document.querySelector('.project').setAttribute('id', 'active');
+//document.querySelector('.contact').removeAttribute('id', 'active');
+//} else if (contact.offsetTop - 800 <= windo) {
+//document.querySelector('.home').removeAttribute('id', 'active');
+//document.querySelector('.Profil').removeAttribute('id', 'active');
+//document.querySelector('.resume').removeAttribute('id', 'active');
+//document.querySelector('.project').removeAttribute('id', 'active');
+//document.querySelector('.contact').setAttribute('id', 'active');
+//}
+//});
